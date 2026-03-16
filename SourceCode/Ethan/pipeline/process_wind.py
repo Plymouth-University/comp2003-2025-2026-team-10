37e1d754ddd1fcc26587e7d4a88a8ddaaeb3af63
yeni-dev <ethan.adg@proton.me>
2026-03-16 00:48:43 +0000

    process wind

--- SourceCode/Ethan/pipeline/process_wind.py
+++ SourceCode/Ethan/pipeline/process_wind.py
@@ -2,6 +2,6 @@ def process_wind_data(csv_file, output_folder):
 
     print("Processing wind data...")
 
-    # convert wind CSV to JSON or format used by Unity
+    # convert wind CSV to JSON or format used by Unity.
 
     print("Wind data ready.")
