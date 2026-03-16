5ef2c54bd3029022d8f2ba60d66f2e82dd7f0f63
yeni-dev <ethan.adg@proton.me>
2026-03-16 00:47:42 +0000

    process turbine

--- SourceCode/Ethan/pipeline/process_turbine.py
+++ SourceCode/Ethan/pipeline/process_turbine.py
@@ -3,6 +3,6 @@ def process_turbine_data(csv_file, output_folder):
     print("Processing turbine data...")
 
     # convert turbine simulation data
-    # into format used by Unity
+    # into format used by Unity.
 
     print("Turbine data ready.")
